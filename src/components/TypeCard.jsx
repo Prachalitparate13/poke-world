1429f9febb3a383f27b3c4149000356edb0c5084
Prachalit Parate <prachalitparate13@gmail.com>
2024-10-11 15:34:44 +0530

    initial commit

---
+++ src/components/TypeCard.jsx
@@ -0,0 +1,18 @@
+import { pokemonTypeColors } from "../utils/index";
+function TypeCard(props) {
+  const { type } = props;
+  return (
+    <div
+      className="type-tile"
+      style={{
+        color: pokemonTypeColors?.[type]?.color,
+        background: pokemonTypeColors?.[type]?.background,
+      }}
+    >
+      <p>{type}</p>
+    </div>
+    // <div></div>
+  );
+}
+
+export default TypeCard;
